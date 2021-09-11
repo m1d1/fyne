e8a2489da22760d9be040b3db28b647cc2be007f
michael <michael.dinkelaker@gmail.com>
2021-09-11 09:56:01 +0200

    Added new animation motion types: Back, Bounce, Circular, Cubic, Elastic, Exponential, Quadratic, Quartic, Quintic and Sine

--- animation.go
+++ animation.go
@@ -1,6 +1,9 @@
 package fyne
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 // AnimationCurve represents an animation algorithm for calculating the progress through a timeline.
 // Custom animations can be provided by implementing the "func(float32) float32" definition.
@@ -30,6 +33,48 @@ var (
 	//
 	// Since: 2.0
 	AnimationLinear = animationLinear
+
+	// These animation types are adapted from Robert Penner's easing functions.
+	// http://robertpenner.com/easing/
+	AnimationBackEaseInOut = animationBackEaseInOut
+	AnimationBackEaseIn    = animationBackEaseIn
+	AnimationBackEaseOut   = animationBackEaseOut
+
+	AnimationBounceEaseInOut = animationBounceEaseInOut
+	AnimationBounceEaseIn    = animationBounceEaseIn
+	AnimationBounceEaseOut   = animationBounceEaseOut
+
+	AnimationCircularEaseInOut = animationCircularEaseInOut
+	AnimationCircularEaseIn    = animationCircularEaseIn
+	AnimationCircularEaseOut   = animationCircularEaseOut
+
+	AnimationCubicEaseInOut = animationCubicEaseInOut
+	AnimationCubicEaseIn    = animationCubicEaseIn
+	AnimationCubicEaseOut   = animationCubicEaseOut
+
+	AnimationElasticEaseInOut = animationElasticEaseInOut
+	AnimationElasticEaseIn    = animationElasticEaseIn
+	AnimationElasticEaseOut   = animationElasticEaseOut
+
+	AnimationExponentialEaseInOut = animationExponentialEaseInOut
+	AnimationExponentialEaseIn    = animationExponentialEaseIn
+	AnimationExponentialEaseOut   = animationExponentialEaseOut
+
+	AnimationQuadraticEaseInOut = animationQuadaticEaseInOut
+	AnimationQuadraticEaseIn    = animationQuadaticEaseIn
+	AnimationQuadraticEaseOut   = animationQuadaticEaseOut
+
+	AnimationQuarticEaseInOut = animationQuarticEaseInOut
+	AnimationQuarticEaseIn    = animationQuarticEaseIn
+	AnimationQuarticEaseOut   = animationQuarticEaseOut
+
+	AnimationQuinticEaseInOut = animationQuinticEaseInOut
+	AnimationQuinticEaseIn    = animationQuinticEaseIn
+	AnimationQuinticEaseOut   = animationQuinticEaseOut
+
+	AnimationSineEaseInOut = animationSineEaseInOut
+	AnimationSineEaseIn    = animationSineEaseIn
+	AnimationSineEaseOut   = animationSineEaseOut
 )
 
 // Animation represents an animated element within a Fyne canvas.
@@ -71,7 +116,6 @@ func animationEaseInOut(val float32) float32 {
 	if val <= 0.5 {
 		return val * val * 2
 	}
-
 	return -1 + (4-val*2)*val
 }
 
@@ -81,4 +125,226 @@ func animationEaseOut(val float32) float32 {
 
 func animationLinear(val float32) float32 {
 	return val
+}
+
+// Back
+func animationBackEaseIn(val float32) float32 {
+	var s float32 = 1.70158
+	return val * val * ((s+1)*val - s)
+}
+
+func animationBackEaseOut(val float32) float32 {
+	var s float32 = 1.70158
+	val -= 1
+	return val*val*((s+1)*val+s) + 1
+}
+
+func animationBackEaseInOut(val float32) float32 {
+	var s float32 = 1.70158
+	var r float32 = 1.525
+	//return ((ratio *= 2) < 1) ? 0.5*(ratio*ratio*((s*1.525+1)*ratio-s*1.525)) : 0.5*((ratio -= 2)*ratio*((s*1.525+1)*ratio+s*1.525)+2);
+	val *= 2
+	if val < 1 {
+		return 0.5 * (val * val * ((s*r+1)*val - s*r))
+	}
+	val -= 2
+	return 0.5 * ((val)*val*((s*r+1)*val+s*r) + 2)
+}
+
+// Bounce
+func animationBounceEaseIn(val float32) float32 {
+	return 1 - animationBounceEaseOut(1-val)
+}
+
+func animationBounceEaseOut(val float32) float32 {
+	var s float32 = 7.5625
+	var r float32 = 2.75
+	if val < 1/r {
+		return s * val * val
+	} else if val < 2/r {
+		val -= 1.5 / r
+		return s*val*val + 0.75
+	} else if val < 2.5/r {
+		val -= 2.25 / r
+		return s*val*val + 0.9375
+	}
+	val -= 2.625 / r
+	return s*val*val + 0.984375
+}
+
+func animationBounceEaseInOut(val float32) float32 {
+	val *= 2
+	if val < 1 {
+		return 0.5 * animationBounceEaseIn(val)
+	}
+	return 0.5*animationBounceEaseOut(val-1) + 0.5
+}
+
+// Circular
+func animationCircularEaseIn(val float32) float32 {
+	val2 := float64(val)
+	return float32(-(math.Sqrt(1-val2*val2) - 1))
+}
+
+func animationCircularEaseOut(val float32) float32 {
+	val2 := float64(val)
+	return float32(math.Sqrt(1 - (val2-1)*(val2-1)))
+}
+
+func animationCircularEaseInOut(val float32) float32 {
+	val *= 2
+	val2 := float64(val)
+	if val2 < 1 {
+		return float32(-0.5 * (math.Sqrt(1-val2*val2) - 1))
+	}
+	val2 -= 2
+	return float32(0.5 * (math.Sqrt(1-val2*val2) + 1))
+}
+
+// Cubic
+func animationCubicEaseIn(val float32) float32 {
+	return val * val * val
+}
+func animationCubicEaseOut(val float32) float32 {
+	val -= 1
+	return val*val*val + 1
+}
+func animationCubicEaseInOut(val float32) float32 {
+	if val < 0.5 {
+		return 4 * val * val * val
+	}
+	val -= 1
+	return 4*val*val*val + 1
+}
+
+// Elastic
+func animationElasticEaseIn(val float32) float32 {
+	var a float64 = 1
+	var p float64 = 0.3
+	var s float64 = p / 4
+
+	if val == 0 || val == 1 {
+		return val
+	}
+	val -= 1
+	val2 := float64(val)
+	return float32(-(a * math.Pow(2, 10*val2) * math.Sin((val2-s)*(2*math.Pi)/p)))
+
+}
+func animationElasticEaseOut(val float32) float32 {
+	var a float64 = 1
+	var p float64 = 0.3
+	var s float64 = p / 4
+
+	if val == 0 || val == 1 {
+		return val
+	}
+	val2 := float64(val)
+	return float32(a*math.Pow(2, -10*val2)*math.Sin((val2-s)*(2*math.Pi)/p) + 1)
+}
+func animationElasticEaseInOut(val float32) float32 {
+
+	var a float64 = 1
+	var p float64 = 0.3
+	var s float64 = p / 4
+
+	if val == 0 || val == 1 {
+		return val
+	}
+	val = val*2 - 1
+	val2 := float64(val)
+	if val < 0 {
+		return float32(-0.5 * (a * math.Pow(2, 10*val2) * math.Sin((val2-s*1.5)*(2*math.Pi)/(p*1.5))))
+	}
+	return float32(0.5*a*math.Pow(2, -10*val2)*math.Sin((val2-s*1.5)*(2*math.Pi)/(p*1.5)) + 1)
+}
+
+// Exponential
+func animationExponentialEaseIn(val float32) float32 {
+	if val == 0 {
+		return 0
+	}
+	val2 := float64(val)
+	return float32(math.Pow(2, 10*(val2-1)))
+}
+
+func animationExponentialEaseOut(val float32) float32 {
+	if val == 1 {
+		return 1
+	}
+	val2 := float64(val)
+	return float32(1 - math.Pow(2, -10*val2))
+}
+
+func animationExponentialEaseInOut(val float32) float32 {
+	if val == 0 || val == 1 {
+		return val
+	}
+	val = val*2 - 1
+	val2 := float64(val)
+	if 0 > val {
+		return float32(0.5 * math.Pow(2, 10*val2))
+	}
+	return float32(1 - 0.5*math.Pow(2, -10*val2))
+}
+
+// // Quadratic
+func animationQuadaticEaseIn(val float32) float32 {
+	return val * val
+}
+func animationQuadaticEaseOut(val float32) float32 {
+	return -val * (val - 2)
+}
+func animationQuadaticEaseInOut(val float32) float32 {
+	if val < 0.5 {
+		return 2 * val * val
+	}
+	return -2*val*(val-2) - 1
+}
+
+// // Quartic
+func animationQuarticEaseIn(val float32) float32 {
+	return val * val * val * val
+}
+func animationQuarticEaseOut(val float32) float32 {
+	val -= 1
+	return 1 - val*val*val*val
+}
+func animationQuarticEaseInOut(val float32) float32 {
+	if val < 0.5 {
+		return 8 * val * val * val * val
+	}
+	val -= 1
+	return -8*val*val*val*val + 1
+
+}
+
+// // Quintic
+func animationQuinticEaseIn(val float32) float32 {
+	return val * val * val * val * val
+}
+func animationQuinticEaseOut(val float32) float32 {
+	val -= 1
+	return 1 + val*val*val*val*val
+}
+func animationQuinticEaseInOut(val float32) float32 {
+	if val < 0.5 {
+		return 16 * val * val * val * val * val
+	}
+	val -= 1
+	return 16*val*val*val*val*val + 1
+}
+
+// Sine
+func animationSineEaseIn(val float32) float32 {
+	val2 := float64(val)
+	return float32(1 - math.Cos(val2*(math.Pi/2)))
+}
+func animationSineEaseOut(val float32) float32 {
+	val2 := float64(val)
+	return float32(math.Sin(val2 * (math.Pi / 2)))
+}
+func animationSineEaseInOut(val float32) float32 {
+	val2 := float64(val)
+	return float32(-0.5 * (math.Cos(val2*math.Pi) - 1))
 }
--- cmd/fyne_demo/tutorials/animation.go
+++ cmd/fyne_demo/tutorials/animation.go
@@ -10,6 +10,8 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+var a0 *fyne.Animation
+
 func makeAnimationScreen(_ fyne.Window) fyne.CanvasObject {
 	curves := makeAnimationCurves()
 	curves.Move(fyne.NewPos(0, 140+theme.Padding()))
@@ -29,29 +31,28 @@ func makeAnimationCanvas() fyne.CanvasObject {
 	a.AutoReverse = true
 	a.Start()
 
-	var a2 *fyne.Animation
 	i := widget.NewIcon(theme.CheckButtonCheckedIcon())
-	a2 = canvas.NewPositionAnimation(fyne.NewPos(0, 0), fyne.NewPos(350, 80), time.Second*3, func(p fyne.Position) {
+	a0 = canvas.NewPositionAnimation(fyne.NewPos(0, 0), fyne.NewPos(350, 80), time.Second*3, func(p fyne.Position) {
 		i.Move(p)
 
 		width := 10 + (p.X / 7)
 		i.Resize(fyne.NewSize(width, width))
 	})
-	a2.RepeatCount = fyne.AnimationRepeatForever
-	a2.AutoReverse = true
-	a2.Curve = fyne.AnimationLinear
-	a2.Start()
+	a0.RepeatCount = fyne.AnimationRepeatForever
+	a0.AutoReverse = true
+	a0.Curve = fyne.AnimationLinear
+	a0.Start()
 
 	running := true
 	var toggle *widget.Button
 	toggle = widget.NewButton("Stop", func() {
 		if running {
 			a.Stop()
-			a2.Stop()
+			a0.Stop()
 			toggle.SetText("Start")
 		} else {
 			a.Start()
-			a2.Start()
+			a0.Start()
 			toggle.SetText("Stop")
 		}
 		running = !running
@@ -62,20 +63,98 @@ func makeAnimationCanvas() fyne.CanvasObject {
 }
 
 func makeAnimationCurves() fyne.CanvasObject {
-	label1, box1, a1 := makeAnimationCurveItem("EaseInOut", fyne.AnimationEaseInOut, 0)
-	label2, box2, a2 := makeAnimationCurveItem("EaseIn", fyne.AnimationEaseIn, 30+theme.Padding())
-	label3, box3, a3 := makeAnimationCurveItem("EaseOut", fyne.AnimationEaseOut, 60+theme.Padding()*2)
-	label4, box4, a4 := makeAnimationCurveItem("Linear", fyne.AnimationLinear, 90+theme.Padding()*3)
+	var (
+		yOff1 float32 = 50
+		yOff2 float32 = 80 + theme.Padding()
+		yOff3 float32 = 110 + theme.Padding()*2
+	)
 
+	label1, box1, a1 := makeAnimationCurveItem("EaseInOut", fyne.AnimationLinear, yOff1)
+	label2, box2, a2 := makeAnimationCurveItem("EaseIn", fyne.AnimationLinear, yOff2)
+	label3, box3, a3 := makeAnimationCurveItem("EaseOut", fyne.AnimationLinear, yOff3)
+
+	combo := widget.NewSelect([]string{"Linear", "Ease", "Back", "Bounce", "Circular", "Cubic", "Elastic", "Exponential", "Quadratic", "Quartic", "Quintic", "Sine"}, func(value string) {
+		if a0 == nil {
+			// catch early combo.SetSelectedIndex(0)
+			return
+		}
+
+		switch value {
+		case "Linear":
+			a0.Curve = fyne.AnimationLinear
+			a1.Curve = fyne.AnimationLinear
+			a2.Curve = fyne.AnimationLinear
+			a3.Curve = fyne.AnimationLinear
+		case "Ease":
+			a0.Curve = fyne.AnimationEaseOut
+			a1.Curve = fyne.AnimationEaseInOut
+			a2.Curve = fyne.AnimationEaseIn
+			a3.Curve = fyne.AnimationEaseOut
+		case "Back":
+			a0.Curve = fyne.AnimationBackEaseOut
+			a1.Curve = fyne.AnimationBackEaseInOut
+			a2.Curve = fyne.AnimationBackEaseIn
+			a3.Curve = fyne.AnimationBackEaseOut
+		case "Bounce":
+			a0.Curve = fyne.AnimationBounceEaseOut
+			a1.Curve = fyne.AnimationBounceEaseInOut
+			a2.Curve = fyne.AnimationBounceEaseIn
+			a3.Curve = fyne.AnimationBounceEaseOut
+		case "Circular":
+			a0.Curve = fyne.AnimationCircularEaseOut
+			a1.Curve = fyne.AnimationCircularEaseInOut
+			a2.Curve = fyne.AnimationCircularEaseIn
+			a3.Curve = fyne.AnimationCircularEaseOut
+		case "Cubic":
+			a0.Curve = fyne.AnimationCubicEaseOut
+			a1.Curve = fyne.AnimationCubicEaseInOut
+			a2.Curve = fyne.AnimationCubicEaseIn
+			a3.Curve = fyne.AnimationCubicEaseOut
+		case "Elastic":
+			a0.Curve = fyne.AnimationElasticEaseOut
+			a1.Curve = fyne.AnimationElasticEaseInOut
+			a2.Curve = fyne.AnimationElasticEaseIn
+			a3.Curve = fyne.AnimationElasticEaseOut
+		case "Exponential":
+			a0.Curve = fyne.AnimationExponentialEaseOut
+			a1.Curve = fyne.AnimationExponentialEaseInOut
+			a2.Curve = fyne.AnimationExponentialEaseIn
+			a3.Curve = fyne.AnimationExponentialEaseOut
+		case "Quadratic":
+			a0.Curve = fyne.AnimationQuadraticEaseOut
+			a1.Curve = fyne.AnimationQuadraticEaseInOut
+			a2.Curve = fyne.AnimationQuadraticEaseIn
+			a3.Curve = fyne.AnimationQuadraticEaseOut
+		case "Quartic":
+			a0.Curve = fyne.AnimationQuarticEaseOut
+			a1.Curve = fyne.AnimationQuarticEaseInOut
+			a2.Curve = fyne.AnimationQuarticEaseIn
+			a3.Curve = fyne.AnimationQuarticEaseOut
+		case "Quintic":
+			a0.Curve = fyne.AnimationQuinticEaseOut
+			a1.Curve = fyne.AnimationQuinticEaseInOut
+			a2.Curve = fyne.AnimationQuinticEaseIn
+			a3.Curve = fyne.AnimationQuinticEaseOut
+		case "Sine":
+			a0.Curve = fyne.AnimationQuinticEaseOut
+			a1.Curve = fyne.AnimationSineEaseInOut
+			a2.Curve = fyne.AnimationSineEaseIn
+			a3.Curve = fyne.AnimationSineEaseOut
+
+		}
+	})
+
+	combo.Resize(combo.MinSize())
+	combo.Move(fyne.NewPos(0, 0+theme.Padding()))
+	combo.SetSelectedIndex(0)
 	start := widget.NewButton("Compare", func() {
 		a1.Start()
 		a2.Start()
 		a3.Start()
-		a4.Start()
 	})
 	start.Resize(start.MinSize())
-	start.Move(fyne.NewPos(0, 120+theme.Padding()*4))
-	return fyne.NewContainerWithoutLayout(label1, label2, label3, label4, box1, box2, box3, box4, start)
+	start.Move(fyne.NewPos(0, 150+theme.Padding()))
+	return fyne.NewContainerWithoutLayout(label1, label2, label3, box1, box2, box3, start, combo)
 }
 
 func makeAnimationCurveItem(label string, curve fyne.AnimationCurve, yOff float32) (
@@ -89,13 +168,13 @@ func makeAnimationCurveItem(label string, curve fyne.AnimationCurve, yOff float3
 	box.Move(fyne.NewPos(0, yOff))
 
 	anim = canvas.NewPositionAnimation(
-		fyne.NewPos(0, yOff), fyne.NewPos(380, yOff), time.Second, func(p fyne.Position) {
+		fyne.NewPos(0, yOff), fyne.NewPos(380, yOff), time.Millisecond*1500, func(p fyne.Position) {
 			box.Move(p)
 			box.Refresh()
 		})
 	anim.Curve = curve
-	anim.AutoReverse = true
-	anim.RepeatCount = 1
+	anim.AutoReverse = false
+	anim.RepeatCount = 0
 	return
 }
 
